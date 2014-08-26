0c57611ec96533aedfb1bdbc87a0f0d175cfe541
Richard Nienaber <rjnienaber@gmail.com>
2014-08-26 22:14:52 +0100

    Fix travis ci build issues

--- frameworks/Ruby/rails/install.sh
+++ frameworks/Ruby/rails/install.sh
@@ -9,4 +9,5 @@ rvm install jruby-1.7.13
 rvm jruby-1.7.13 do bundle install --gemfile=$TROOT/Gemfile
 
 rvm install rbx-2.2.10
+rvm rbx-2.2.10 do rvm --force gemset empty
 rvm rbx-2.2.10 do bundle install --gemfile=$TROOT/Gemfile
