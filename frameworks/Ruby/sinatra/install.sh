dd60f1339b83e75a2c3104b54c9f8550b1462624
Richard Nienaber <rjnienaber@gmail.com>
2014-08-28 22:55:08 +0100

    Try messing with bundler locations

--- frameworks/Ruby/sinatra/install.sh
+++ frameworks/Ruby/sinatra/install.sh
@@ -5,11 +5,7 @@ fw_depends rvm nginx java
 rvm install rbx-2.2.10
 # Some gems seem to fail when installing on travis
 # clearing at the cache so it always retrieves new ones
-if [ "$TRAVIS" == "true" ]
-then
-  rm -rf /home/travis/.rvm/gems/rbx-2.2.10/cache
-fi
-rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
+rvm rbx-2.2.10 do bundle install --path=$TROOT/vendor --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
 
 rvm install ruby-2.0.0-p0
 rvm ruby-2.0.0-p0 do bundle install --gemfile=$TROOT/Gemfile
