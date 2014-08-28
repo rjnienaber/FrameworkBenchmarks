cde5ff4a572a8489ffb85e76de016c355b48bbf7
Richard Nienaber <rjnienaber@gmail.com>
2014-08-28 23:39:05 +0100

    Experiment with adding gem files

--- frameworks/Ruby/padrino/install.sh
+++ frameworks/Ruby/padrino/install.sh
@@ -4,8 +4,9 @@ fw_depends rvm nginx java
 
 # Some gems seem to fail when installing on travis
 # clearing at the cache so it always retrieves new ones
-rm -rf /home/travis/.rvm/gems/rbx-2.2.10/cache
 rvm install rbx-2.2.10
+rvm rbx-2.2.10@global do gem uninstall bundler --force -x
+rvm rbx-2.2.10 do gem install bundler -v=1.6.2
 rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
 
 rvm install ruby-2.0.0-p0
--- frameworks/Ruby/rails/install.sh
+++ frameworks/Ruby/rails/install.sh
@@ -5,7 +5,9 @@ fw_depends rvm nginx java
 rvm install rbx-2.2.10
 # Some gems seem to fail when installing on travis
 # clearing at the cache so it always retrieves new ones
-rvm rbx-2.2.10 do bundle install --no-cache --no-prune --path=$TROOT/vendor --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
+rvm rbx-2.2.10 do rvm --force gemset empty
+rvm rbx-2.2.10@global do rvm --force gemset empty
+rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
 
 rvm install 2.1.2
 rvm 2.1.2 do bundle install --gemfile=$TROOT/Gemfile
