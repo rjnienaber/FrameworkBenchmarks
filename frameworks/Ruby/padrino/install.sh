0857864eb3dbd17b1f025c5f00a378e4f39b7f91
Richard Nienaber <rjnienaber@gmail.com>
2014-08-28 19:24:51 +0100

    Clear out rvm cache on TRAVIS

--- frameworks/Ruby/padrino/install.sh
+++ frameworks/Ruby/padrino/install.sh
@@ -2,11 +2,16 @@
 
 fw_depends rvm nginx java
 
+rvm install rbx-2.2.10
+# Some gems seem to fail when installing on travis
+# clearing at the cache so it always retrieves new ones
+[ "$TRAVIS" == "true" ] || { \
+  rm -rf /home/travis/.rvm/gems/rbx-2.2.10/cache
+}
+rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
+
 rvm install ruby-2.0.0-p0
 rvm ruby-2.0.0-p0 do bundle install --gemfile=$TROOT/Gemfile
 
 rvm install jruby-1.7.8
 rvm jruby-1.7.8 do bundle install --gemfile=$TROOT/Gemfile
-
-rvm install rbx-2.2.10
-rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
--- frameworks/Ruby/rails/install.sh
+++ frameworks/Ruby/rails/install.sh
@@ -2,11 +2,16 @@
 
 fw_depends rvm nginx java
 
+rvm install rbx-2.2.10
+# Some gems seem to fail when installing on travis
+# clearing at the cache so it always retrieves new ones
+[ "$TRAVIS" == "true" ] || { \
+  rm -rf /home/travis/.rvm/gems/rbx-2.2.10/cache
+}
+rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
+
 rvm install 2.1.2
 rvm 2.1.2 do bundle install --gemfile=$TROOT/Gemfile
 
 rvm install jruby-1.7.13
 rvm jruby-1.7.13 do bundle install --gemfile=$TROOT/Gemfile
-
-rvm install rbx-2.2.10
-rvm rbx-2.2.10 do bundle install --jobs=1 --retry=3 --gemfile=$TROOT/Gemfile
